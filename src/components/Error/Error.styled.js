48bf7942a9177acd8733e6db0748efabece94ab8
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-11-10 11:58:37 +0200

    error

---
+++ src/components/Error/Error.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Err = styled.div`
+    text-align: center;
+    margin-top: 30px;
+    padding: 10px;
+    width: 400px;
+    font-weight: 500;
+    font-size: 20px;
+    background-color: #fa0e0ef6;
+`;
